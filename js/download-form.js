e26eaa7ac348440773ca79b706c2085616d29e31
Chrisac <chriisac@gmail.com>
2023-04-05 10:00:42 -0300

    add itemDownload function to update download status and optimizations

--- js/download-form.js
+++ js/download-form.js
@@ -1,5 +1,8 @@
 let list = document.getElementById("downloadList");
 const listItems = [];
+const listColumns = { "filename":0,
+                      "progress":1,
+                      "status"  :2};
 
 class ItemDownload
 {
@@ -8,11 +11,14 @@ class ItemDownload
     this.id = newId;
     this.fileName = fileName;
     this.progress = "0";
+    this.status   = "Downloading";
     list.insertAdjacentHTML("beforeend",`<tr id='${this.id}'>
                                               <td>${this.fileName}</td>
                                               <td>${this.progress}%</td>
-                                              <td>Downloading</td>
+                                              <td>${this.status}</td>
                                             </tr>`);
+
+    this.tr = list.querySelectorAll("tr")[this.id];
   }
 
   updateDownloadProgress(percentage)
@@ -20,7 +26,18 @@ class ItemDownload
     if(percentage >=0)
     {
       this.progress = percentage;
-      list.querySelectorAll("tr")[this.id].querySelectorAll("td")[1].innerHTML = this.progress + "%";//select this class tr in the list table, then select the progress td and updated it with the new progress percentage
+      this.tr.querySelectorAll("td")[listColumns.progress].innerHTML = this.progress + "%";
+      return true;
+    }
+    return false;
+  }
+
+  updateDownloadStatus(status)
+  {
+    if(status)
+    {
+      this.status = status;
+      this.tr.querySelectorAll("td")[listColumns.status].innerHTML = this.status;
       return true;
     }
     return false;
